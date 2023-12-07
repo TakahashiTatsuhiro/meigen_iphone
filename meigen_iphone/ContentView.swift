b9fb824d6c93a6db60dd4deb63c6f73027921353
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-07 15:02:22 +0900

    get Quote

--- meigen_iphone/ContentView.swift
+++ meigen_iphone/ContentView.swift
@@ -7,17 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct Quote: Decodable {
+    var id: Int
+    var author: String
+    var body: String
+}
+
+class QuoteViewModel: ObservableObject {
+    @Published var quote: Quote?
+    @Published var isLoading = false
+
+    func fetchQuote() {
+        guard let url = URL(string: "https://meigen-backend.onrender.com/meigen") else {
+            print("Invalid URL")
+            return
         }
-        .padding()
+        isLoading = true
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                if let error = error {
+                    print("Error fetching data: \(error.localizedDescription)")
+                    return
+                }
+                guard let data = data else {
+                    print("No data received")
+                    return
+                }
+                do {
+                    self.quote = try JSONDecoder().decode(Quote.self, from: data)
+                } catch {
+                    print("Error decoding data: \(error.localizedDescription)")
+                }
+            }
+        }.resume()
     }
 }
+
+
+struct ContentView: View {
+    @StateObject var viewModel = QuoteViewModel()
+
+    var body: some View {
+        VStack {
+            Button("Get Quote") {
+                viewModel.fetchQuote()
+            }
+            .padding()
+            if viewModel.isLoading {
+                ProgressView()
+            } else if let quote = viewModel.quote {
+                Text(quote.author)
+                    .padding()
+                Text(quote.body)
+                    .padding()
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
