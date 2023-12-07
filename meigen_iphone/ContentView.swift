bc39a9f2d0d7d19cc7a02be232aeaf6a6afbc369
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-07 11:39:55 +0900

    xcode first

---
+++ meigen_iphone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meigen_iphone
+//
+//  Created by 1460969 on 2023/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meigen_iphone/meigen_iphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  meigen_iphoneApp.swift
+//  meigen_iphone
+//
+//  Created by 1460969 on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct meigen_iphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
