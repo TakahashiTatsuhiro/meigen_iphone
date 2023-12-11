b0e1cf6302bf33273f3963349370d959b373b148
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-11 14:09:22 +0900

    re-commit

--- meigen_iphone/QuoteViewModel.swift
+++ meigen_iphone/QuoteViewModel.swift
@@ -1,18 +1,144 @@
-//
-//  QuoteViewModel.swift
-//  meigen_iphone
-//
-//  Created by 1460969 on 2023/12/08.
-//
-
 import SwiftUI
 
-struct QuoteViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class QuoteViewModel: ObservableObject {
+    @Published var quote: Quote?
+    @Published var authorInfo: String?
+    @Published var authorImageURL: URL?
+    @Published var isLoading = false
+    
+    func fetchQuote() {
+        guard let url = URL(string: "https://meigen-backend.onrender.com/meigen") else {
+            print("Invalid URL")
+            return
+        }
+        isLoading = true
+        authorInfo = nil
+        authorImageURL = nil
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                if let error = error {
+                    print("Error fetching data: \(error.localizedDescription)")
+                    return
+                }
+                guard let data = data else {
+                    print("No data received")
+                    return
+                }
+                do {
+                    self.quote = try JSONDecoder().decode(Quote.self, from: data)
+                    self.fetchAuthorImage()
+                    self.fetchAuthorInfo()
+                } catch {
+                    print("Error decoding data: \(error.localizedDescription)")
+                }
+            }
+        }.resume()
     }
-}
-
-#Preview {
-    QuoteViewModel()
+    
+    func fetchQuoteFromNewAPI() {
+        guard let url = URL(string: "https://meigen.doodlenote.net/api/json.php?c=1") else {
+            print("Invalid URL")
+            return
+        }
+        isLoading = true
+        authorInfo = nil
+        authorImageURL = nil
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                if let error = error {
+                    print("Error fetching data: \(error.localizedDescription)")
+                    return
+                }
+                guard let data = data else {
+                    print("No data received")
+                    return
+                }
+                do {
+                    if let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: String]],
+                       let firstQuote = jsonResult.first,
+                       let body = firstQuote["meigen"],
+                       let author = firstQuote["auther"] {
+                        self.quote = Quote(id: 0, author: author, body: body)
+                        self.fetchAuthorImage()
+                        self.fetchAuthorInfo()
+                    }
+                } catch {
+                    print("Error decoding data: \(error.localizedDescription)")
+                }
+            }
+        }.resume()
+    }
+    
+    func fetchAuthorInfo() {
+        if let author = self.quote?.author {
+            guard let url = URL(string: "https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=\(author)") else {
+                print("Invalid URL")
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                DispatchQueue.main.async {
+                    if let error = error {
+                        print("Error fetching author info: \(error.localizedDescription)")
+                        return
+                    }
+                    guard let data = data else {
+                        print("No data received")
+                        return
+                    }
+                    do {
+                        let result = try JSONDecoder().decode(WikipediaResponse.self, from: data)
+                        if let page = result.query.pages.values.first {
+                            self.authorInfo = page.extract
+                        } else {
+                            self.authorInfo = "著者情報が見つかりませんでした。"
+                        }
+                    } catch {
+                        print("Error decoding author info: \(error.localizedDescription)")
+                    }
+                }
+            }.resume()
+        } else {
+            print("Quote not available")
+        }
+        
+    }
+    
+    func fetchAuthorImage() {
+        if let author = self.quote?.author {
+            guard let url = URL(string: "https://ja.wikipedia.org/api/rest_v1/page/summary/\(encodeURIComponent(author))") else {
+                print("Invalid URL")
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                DispatchQueue.main.async {
+                    if let error = error {
+                        print("Error fetching author image info: \(error.localizedDescription)")
+                        self.authorImageURL = nil
+                        return
+                    }
+                    guard let data = data,
+                          let response = try? JSONDecoder().decode(WikipediaImageResponse.self, from: data),
+                          let imageUrlString = response.thumbnail?.source,
+                          let imageUrl = URL(string: imageUrlString) else {
+                        print("Error decoding author image info or no image found")
+                        self.authorImageURL = nil
+                        return
+                    }
+                    self.authorImageURL = imageUrl
+                }
+            }.resume()
+        } else {
+            print("Author name not available for image fetch")
+            self.authorImageURL = nil
+        }
+    }
+    
+    private func encodeURIComponent(_ string: String) -> String {
+        return string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? string
+    }
+    
 }
--- meigen_widget/meigen_widget.swift
+++ meigen_widget/meigen_widget.swift
@@ -1,80 +1,88 @@
-//
-//  meigen_widget.swift
-//  meigen_widget
-//
-//  Created by 1460969 on 2023/12/08.
-//
-
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+struct Quote: Decodable {
+    var id: Int
+    var author: String
+    var body: String
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let quote: String
+    let author: String
+}
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), quote: "名言", author: "著者")
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), quote: "名言", author: "著者")
+        completion(entry)
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        let currentDate = Date()
+        let refreshDate = Calendar.current.date(byAdding: .minute, value: 15, to: currentDate)!
+        
+        fetchQuote { quote in
+            let entry = SimpleEntry(date: currentDate, quote: quote.body, author: quote.author)
+            let timeline = Timeline(entries: [entry], policy: .after(refreshDate))
+            completion(timeline)
+        }
+    }
+    
+    func fetchQuote(completion: @escaping (Quote) -> Void) {
+        let url = URL(string: "https://meigen-backend.onrender.com/meigen")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let quote = try? JSONDecoder().decode(Quote.self, from: data) {
+                completion(quote)
+            } else {
+                // エラーハンドリング: デフォルトの名言を返すか、エラーを示す
+                completion(Quote(id: 0, author: "不明", body: "名言が取得できませんでした"))
+            }
+        }.resume()
+    }
+    
+    
 }
 
 struct meigen_widgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+        VStack(alignment: .leading) {
+            Text(entry.author)
+                .font(.caption)
+                .foregroundColor(.secondary)
+            Text(entry.quote)
+                .font(.headline)
+                .bold()
+                .foregroundColor(.primary)
         }
+        .padding()
+        .cornerRadius(10) // 角を丸める
+        .containerBackground(.fill.tertiary, for: .widget) // ここに修飾子を追加
     }
 }
 
 struct meigen_widget: Widget {
     let kind: String = "meigen_widget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                meigen_widgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                meigen_widgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
+            meigen_widgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("名言ウィジェット")
+        .description("定期的に新しい名言を表示します。")
+        .supportedFamilies([.systemMedium]) // ミドルサイズのみをサポート
     }
 }
 
-#Preview(as: .systemSmall) {
+#Preview(as: .systemMedium) {
     meigen_widget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    SimpleEntry(date: .now, quote: "名言", author: "著者")
 }
