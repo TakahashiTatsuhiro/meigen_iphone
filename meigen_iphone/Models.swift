2f1fe1934611cafaee51b77adb56f20dc4ce61ce
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-12-11 13:59:34 +0900

    finish

---
+++ meigen_iphone/Models.swift
@@ -0,0 +1,18 @@
+//
+//  Models.swift
+//  meigen_iphone
+//
+//  Created by 1460969 on 2023/12/08.
+//
+
+import SwiftUI
+
+struct Models: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Models()
+}
---
+++ meigen_iphone/QuoteViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteViewModel.swift
+//  meigen_iphone
+//
+//  Created by 1460969 on 2023/12/08.
+//
+
+import SwiftUI
+
+struct QuoteViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteViewModel()
+}
